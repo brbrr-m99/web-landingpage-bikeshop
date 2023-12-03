d7f4cb16baeb09e2223fd86dc77ab3e4dd211b6b
Mulia Brilian Hotma Tua <muliabrilian99@gmail.com>
2023-12-03 16:52:09 +0700

    update kelima

--- source/js/main-readblog.js
+++ source/js/main-readblog.js
@@ -36,3 +36,21 @@ window.addEventListener("resize", () => {
    }
 });
 
+// scrollBanner
+
+function scrollBanner(config) {
+
+	scrollPos = $(this).scrollTop();
+
+	config.bannerText.css({
+		'margin-top' : -(scrollPos/3) + "px",
+		'opacity' : 1-(scrollPos/300)
+	});
+
+	config.banner.css({
+		'background-position': 'center ' + (-scrollPos/4) + "px"
+	});
+
+}
+
+
