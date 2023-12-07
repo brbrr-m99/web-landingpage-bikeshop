391f6b7e7f189aa73064d358374dafbea886e13e
Mulia Brilian Hotma Tua <muliabrilian99@gmail.com>
2023-12-07 08:50:59 +0700

    update kamis 7 desember 2023

--- page/produk/detail-produk/js/detail-produk.js
+++ page/produk/detail-produk/js/detail-produk.js
@@ -36,3 +36,14 @@ window.addEventListener("resize", () => {
    }
 });
 
+$(document).ready(function() {
+ 
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+ 
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+ 
+});
