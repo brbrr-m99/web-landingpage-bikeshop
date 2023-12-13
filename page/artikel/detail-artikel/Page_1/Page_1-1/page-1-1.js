8082ed6bfb4b70bd0c98c6d2910b7c5c6340da07
Mulia Brilian Hotma Tua <muliabrilian99@gmail.com>
2023-12-13 16:39:34 +0700

    update

--- page/artikel/detail-artikel/Page_1/Page_1-1/page-1-1.js
+++ page/artikel/detail-artikel/Page_1/Page_1-1/page-1-1.js
@@ -36,21 +36,3 @@ window.addEventListener("resize", () => {
    }
 });
 
-// scrollBanner
-
-function scrollBanner(config) {
-
-	scrollPos = $(this).scrollTop();
-
-	config.bannerText.css({
-		'margin-top' : -(scrollPos/3) + "px",
-		'opacity' : 1-(scrollPos/300)
-	});
-
-	config.banner.css({
-		'background-position': 'center ' + (-scrollPos/4) + "px"
-	});
-
-}
-
-
