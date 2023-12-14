8da3ba6f7efd984ec658701b4de689cc3174f74a
Mulia Brilian Hotma Tua <muliabrilian99@gmail.com>
2023-12-14 09:47:23 +0700

    update

--- page/lokasi/lokasi.js
+++ page/lokasi/lokasi.js
@@ -46,27 +46,3 @@ function activate(e) {
 }
 
 document.addEventListener('click',activate,false);
-
-// Logo Slider
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
